ec8b23d841cd2411b7a80120d32d851cdcc6dc9e
Jason <bjason@gmail.com>
2014-03-01 05:10:19 -0800

    simple python version

--- exercises/euler/001.js
+++ exercises/euler/001.js
@@ -4,15 +4,19 @@
 //  A very naive way of solving:
 //  Just looping over the values and doing mod to find
 //  values that have no reminders, hence perfectly divide
-var runnigSum = 0;
-for(var ii = 0 ; ii< 100; ii++){
-    if (ii % 3 === 0){
-        runnigSum+=ii;
-    }
+function getSum(upTo){
+    var runnigSum = 0;
+    for(var ii = 0 ; ii < upTo; ii++){
+        if (ii % 3 === 0){
+            runnigSum+=ii;
+        }
 
-    if (ii % 5 === 0){
-        runnigSum+=ii;
+        if (ii % 5 === 0){
+            runnigSum+=ii;
+        }
     }
+    return runnigSum
 }
 
-console.log(runnigSum);
+console.log(getSum(10));
+
---
+++ exercises/euler/001.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+
+
+runningTotal = 0
+for xx in range(0, 10):
+    if xx % 3 == 0:
+        runningTotal+=xx
+
+    if xx % 5 == 0:
+        runningTotal+=xx
+
+print runningTotal
+
+
+
+
