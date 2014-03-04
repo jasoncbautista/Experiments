70f9658fb9ece0df6f0053dc853219f5687cb7b6
Jason <bjason@gmail.com>
2014-03-03 21:40:58 -0800

    Erlang basics.

--- exercises/euler/e001.erl
+++ exercises/euler/e001.erl
@@ -1,6 +1,11 @@
 -module(e001).
 -compile(export_all).
 
+getValues(Count, Curr) when Curr == 1 ->
+    Count;
+
+getValues(Count, Curr)->
+    getValues(Count + 2, Curr - 1).
 
 start() ->
     io:fwrite("testing\n")
