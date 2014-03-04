cff15b8963c0afb8285bfeb8bfcd0d68eff4e128
Jason <bjason@gmail.com>
2014-03-03 21:27:11 -0800

    E001 C# complete

--- mono/Hello/HelloWorld/HelloWorld/Main.cs
+++ mono/Hello/HelloWorld/HelloWorld/Main.cs
@@ -7,6 +7,20 @@ namespace HelloWorld
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+			var total = calculateTo();
+			Console.WriteLine (total);
 		}
+		
+		public static int calculateTo(){
+			var upTo = 10;
+			var count = 0;
+			for(var ii = 0; ii < upTo; ii++){
+				if( ii % 3 == 0 || ii % 5 == 0 ){
+					count+=ii;
+				}
+			}
+			return count;
+		}
+		 
 	}
 }
