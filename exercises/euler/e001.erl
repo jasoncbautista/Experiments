c7a2ceb9a4f7c88827dab1dac49bf77de67dd47a
Jason <bjason@gmail.com>
2014-03-03 21:52:25 -0800

    Euler 001

--- exercises/euler/e001.erl
+++ exercises/euler/e001.erl
@@ -1,11 +1,23 @@
 -module(e001).
 -compile(export_all).
 
-getValues(Count, Curr) when Curr == 1 ->
+getValues(Count, Curr) when (Curr =< 0) ->
     Count;
 
+
+
+getValues(Count, Curr) when Curr rem  3 == 0 ->
+    getValues(Count + Curr, Curr - 1);
+
+getValues(Count, Curr) when Curr rem  5 == 0 ->
+    getValues(Count + Curr, Curr - 1);
+
+
 getValues(Count, Curr)->
-    getValues(Count + 2, Curr - 1).
+    getValues(Count, Curr - 1).
+
+getTotalTo(Max) ->
+    getValues(0, Max-1).
 
 start() ->
     io:fwrite("testing\n")
